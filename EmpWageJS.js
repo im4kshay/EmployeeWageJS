3453bb0cab19b70c119ffe0a4ade799e36c00ba1
im4kshay <akshaysayre7@gmail.com>
2022-05-17 08:14:50 +0530

    Introduce function to get working hrs

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -2,19 +2,22 @@ console.log("=-=-=-=-=-Welcome to the Employee Wage Problem-=-=-=-=-=");
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const EMP_RATE_PER_HR = 20;
+const FULL_TIME_HRS = 8;
+const PART_TIME_HRS = 4;
 let empWage = 0;
-let empCheck = Math.floor(Math.random()*10)%3;
-switch(empCheck)
+function getWorkHrs(empCheck)
 {
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    default:
-        empHrs = 0;
-        break;
+    switch(empCheck)
+    {
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
+        default:
+            return 0;
+    }
 }
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkHrs(empCheck);
 empWage += EMP_RATE_PER_HR*empHrs;
-console.log("Employee wage :"+ empWage);
+console.log("Employee wage : "+ empWage);
