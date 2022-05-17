147c9178e93e5f37cc767be65ed13598d642012f
im4kshay <akshaysayre7@gmail.com>
2022-05-17 08:09:29 +0530

    Method to calculate wage for a day

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -1,11 +1,20 @@
 console.log("=-=-=-=-=-Welcome to the Employee Wage Problem-=-=-=-=-=");
-const IS_PRESENT = 1;
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck=IS_PRESENT)
-    {
-        console.log("Employee is Present");
-    }
-else
-    {
-        console.log("Employee is Absent");
-    }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const EMP_RATE_PER_HR = 20;
+let empWage = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck)
+{
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
+        break;
+}
+empWage += EMP_RATE_PER_HR*empHrs;
+console.log("Employee wage :"+ empWage);
