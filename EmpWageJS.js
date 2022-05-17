c2a5efbec69ae468ebec3b21b08ecbac6c4cff88
im4kshay <akshaysayre7@gmail.com>
2022-05-17 08:26:26 +0530

    Calculating wage for a month

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -4,7 +4,10 @@ const IS_FULL_TIME = 2;
 const EMP_RATE_PER_HR = 20;
 const FULL_TIME_HRS = 8;
 const PART_TIME_HRS = 4;
+const MAX_WORKING_DAYS = 20;
 let empWage = 0;
+let totalWorkingHrs = 0;
+let totalEmpWage = 0;
 function getWorkHrs(empCheck)
 {
     switch(empCheck)
@@ -17,7 +20,14 @@ function getWorkHrs(empCheck)
             return 0;
     }
 }
-let empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkHrs(empCheck);
-empWage += EMP_RATE_PER_HR*empHrs;
-console.log("Employee wage : "+ empWage);
+let empHrs =0;
+for(let day = 1; day <= MAX_WORKING_DAYS; day++)
+{
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHrs = getWorkHrs(empCheck);
+    empWage = EMP_RATE_PER_HR * empHrs;
+    console.log("Emp Wage for day "+day+" is "+empWage);
+    totalWorkingHrs += empHrs;
+}
+totalEmpWage = EMP_RATE_PER_HR*totalWorkingHrs;
+console.log("Total Employee wage for a Month: "+ totalEmpWage);
