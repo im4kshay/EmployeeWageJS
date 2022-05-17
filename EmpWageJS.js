171d2aabe11f7feef7c38fdd8754cd2a79f08f4b
im4kshay <akshaysayre7@gmail.com>
2022-05-17 08:03:52 +0530

    Method to check employee attendance

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -1,1 +1,11 @@
 console.log("=-=-=-=-=-Welcome to the Employee Wage Problem-=-=-=-=-=");
+const IS_PRESENT = 1;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck=IS_PRESENT)
+    {
+        console.log("Employee is Present");
+    }
+else
+    {
+        console.log("Employee is Absent");
+    }
